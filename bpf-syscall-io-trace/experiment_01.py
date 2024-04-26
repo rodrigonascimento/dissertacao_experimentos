f103f33379c70d131ec2fbf6afcba28289c053e0
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-04-26 17:06:52 -0300

    ref: added a few outputs for verbosite.

--- bpf-syscall-io-trace/experiment_01.py
+++ bpf-syscall-io-trace/experiment_01.py
@@ -32,8 +32,9 @@ def create_file_pwrite(file_name: Path, file_size_bytes: int, chunk_size_bytes: 
 def main():
     file_name = Path('./output_experiment_01.bdata')    
     file_size_bytes = 10_485_760
-    chunk_size_bytes = 1_024
+    chunk_size_bytes = 1_048_576
 
+    print(f'Creating file..........: {file_name.name}')
     create_file_pwrite(file_name=file_name, file_size_bytes=file_size_bytes, chunk_size_bytes=chunk_size_bytes)
 
 if __name__ == '__main__':
