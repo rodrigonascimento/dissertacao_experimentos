ef9c3b4615b42b55b34bff6682351b5dfeeccca3
Nascimento, Rodrigo <rodrigon@netapp.com>
2024-04-26 18:57:33 -0300

    ref: added experiment_02.

---
+++ bpf-syscall-io-trace/experiment_02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+
+from pathlib import Path
+
+def main():
+    file_name = Path('./output_experiment_01.bdata')    
+    file_size_bytes = 10_485_760
+    chunk_size_bytes = 1_048_576
+
+    fd = os.open(file_name, os.O_RDWR)
+    print(f'Reading file..........offset 0')
+    os.pread(fd, chunk_size_bytes, 0)
+    print(f'Reading file..........offset {int(file_size_bytes/2)}')
+    os.pread(fd, chunk_size_bytes, int(file_size_bytes/2))
+    print(f'Reading file..........offset {file_size_bytes-chunk_size_bytes}')
+    os.pread(fd, chunk_size_bytes, file_size_bytes-chunk_size_bytes)
+    os.close(fd)
+
+if __name__ == '__main__':
+    main()
